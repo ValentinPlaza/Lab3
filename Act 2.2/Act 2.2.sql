860c702686e4313181da1c2326fc98762f1f0fdf
ValentinPlaza <valentin.plaza@alumnos.frgp.utn.edu.ar>
2021-04-25 23:59:33 -0300

    Act 2.2 Completo, Carga de datos y base de datos completa

--- Act 2.2/Act 2.2.sql
+++ Act 2.2/Act 2.2.sql
@@ -1,3 +1,5 @@
+USE Blueprint
+GO
 --PUNTO 1
 SELECT C.RazonSocial, C.CUIT, T.Descripcion FROM Clientes AS C INNER JOIN TipoCliente AS T
 ON C.IDTipoCliente = T.ID
@@ -58,4 +60,116 @@ WHERE PA.Nombre in ('Argentina', 'Italia')
 SELECT MD.Nombre, MD.CostoEstimado, PR.Nombre, PR.Descripcion, PR.CostoEstimado FROM Modulos AS MD
 	LEFT JOIN Proyectos AS PR
 ON MD.IDProyecto = PR.ID
-WHERE PR.FechaFin IS NOT NULL OR PR.FechaFin < GETDATE()
+WHERE PR.FechaFin IS NOT NULL AND PR.FechaFin < GETDATE()
+
+--PUNTO 9
+SELECT M.Nombre AS nombreModulo, P.Nombre AS nombreProyecto FROM Modulos AS M 
+	INNER JOIN Proyectos AS P
+ON M.IDProyecto = P.id
+WHERE M.TiempoEstimado > 100
+
+--PUNTO 10
+SELECT M.Nombre AS nombreModulo, P.Nombre AS nombreProyecto, M.TiempoEstimado FROM Modulos AS M
+	INNER JOIN Proyectos AS P
+ON M.IDProyecto = P.ID
+WHERE FechaEstimadaFin > M.FechaFin AND P.CostoEstimado > 100000
+
+--PUNTO 11
+SELECT DISTINCT P.Nombre FROM Proyectos AS P
+	INNER JOIN Modulos AS M
+ON M.IDProyecto = P.Id
+WHERE M.FechaFin < M.FechaEstimadaFin
+
+--PUNTO 12
+SELECT DISTINCT CI.Nombre FROM CIUDADES AS CI
+	LEFT JOIN Clientes AS CL
+ON CI.ID = CL.IDCiudad
+	INNER JOIN Colaboradores AS CO
+ON CI.ID = CO.Localidad
+WHERE CO.Localidad IS NOT NULL AND CL.IDCiudad IS NULL
+
+--PUNTO 13
+SELECT P.Nombre AS nombreProyecto, M.Nombre AS nombreModulo FROM Proyectos AS P
+	INNER JOIN Modulos AS M
+ON P.ID = M.IDProyecto
+WHERE M.Nombre LIKE 'Login' OR M.Descripcion LIKE '%Login%'
+
+--PUNTO 14
+SELECT P.Nombre AS nombreProyecto, CO.Nombre, CO.Apellidos FROM Proyectos AS P
+	LEFT JOIN Modulos AS M
+ON P.Id = M.IDProyecto
+	LEFT JOIN Tareas AS T
+ON M.ID = T.IdModulo
+	LEFT JOIN TipoTareas AS TT
+ON TT.ID = T.IdTipoTarea
+	LEFT JOIN Colaboraciones AS C
+ON T.ID = C.IdTarea
+	LEFT JOIN Colaboradores AS CO
+ON CO.ID = C.IdColaborador
+WHERE TT.Nombre LIKE '%Programacion%' OR TT.Nombre LIKE '%Testing%'
+ORDER BY P.Nombre ASC
+
+--PUNTO 15
+SELECT CO.Nombre, CO.Apellidos, M.Nombre AS nombreModulo, TT.Nombre, C.ValorHora, TT.PrecioHoraBase FROM Colaboradores AS CO
+	LEFT JOIN Colaboraciones AS C
+ON CO.ID = C.IdColaborador
+	LEFT JOIN Tareas AS T
+ON C.IdTarea = T.ID
+	LEFT JOIN TipoTareas AS TT
+ON T.IdTipoTarea = TT.ID
+	LEFT JOIN Modulos AS M
+ON T.IdModulo = M.Id
+WHERE C.ValorHora > (TT.PrecioHoraBase/2)
+
+--PUNTO 16
+SELECT TOP (3) CO.Nombre, CO.Apellidos FROM Colaboradores AS CO
+	LEFT JOIN Colaboraciones AS C
+ON CO.ID = C.IdColaborador
+	LEFT JOIN Tareas AS T
+ON C.IdTarea = T.ID
+	LEFT JOIN TipoTareas AS TT
+ON T.IdTipoTarea = TT.ID
+WHERE TT.Nombre LIKE '%Testing%' AND CO.Tipo = 'E'
+ORDER BY C.DuracionHoras DESC
+
+--PUNTO 17
+SELECT CO.Apellidos, CO.Nombre, CO.Mail FROM Colaboradores AS CO
+	INNER JOIN Ciudades AS CI
+ON CO.Localidad = CI.ID
+	LEFT JOIN Paises AS P
+ON CI.IdPaises = P.ID
+WHERE P.Nombre LIKE 'Argentina' AND CO.Tipo LIKE 'I' AND CO.Mail LIKE '%.com%'
+
+--PUNTO 18
+SELECT P.Nombre AS nombreProycto, M.Nombre AS nombreModulo, TT.Nombre AS TipoTarea FROM Proyectos AS P
+	LEFT JOIN Modulos AS M
+ON P.ID = M.IDProyecto
+	LEFT JOIN Tareas AS T
+ON M.ID = T.IdModulo
+	LEFT JOIN TipoTareas AS TT
+ON T.IdTipoTarea = TT.ID
+	LEFT JOIN Colaboraciones AS C
+ON T.ID = C.IdTarea
+	LEFT JOIN Colaboradores AS CO
+ON C.IdColaborador	= CO.ID
+WHERE CO.Tipo LIKE 'E'
+
+--PUNTO 19
+SELECT P.Nombre FROM Proyectos AS P
+	INNER JOIN Modulos AS M
+ON P.ID = M.IDProyecto
+	INNER JOIN Tareas AS T
+ON M.ID = T.IdModulo
+WHERE T.ID IS NULL
+
+--PUNTO 20
+SELECT DISTINCT CO.Nombre, CO.Apellidos FROM Colaboradores AS CO
+	INNER JOIN Colaboraciones AS C
+ON CO.ID = C.IdColaborador
+	INNER JOIN Tareas AS T
+ON C.IdTarea = T.ID
+	INNER JOIN Modulos AS M
+ON T.IdModulo = M.ID
+	INNER JOIN Proyectos AS P
+ON M.IDProyecto = P.ID
+WHERE P.FechaFin IS NULL
---
+++ BluePrint Completo/BLUEPRINT_DB.sql
@@ -0,0 +1,112 @@
+CREATE DATABASE Blueprint1
+GO
+
+USE Blueprint1
+GO
+
+CREATE TABLE TiposCliente(
+	ID int not null primary key identity (1, 1),
+	Nombre varchar(30) not null,	
+)
+GO
+
+CREATE TABLE Clientes(
+	ID int not null primary key identity(1,1),
+	RazonSocial varchar(50) not null,
+	IDTipo int not null foreign key references TiposCliente(ID),
+	CUIT varchar(13) not null unique,
+	EMail varchar(40) null,
+	Telefono varchar(16) null,
+	Celular varchar(16) null
+)
+GO
+
+CREATE TABLE Proyectos(
+	ID varchar(5) not null primary key,
+	IDCliente int not null foreign key references Clientes(ID),
+	Nombre varchar(50) not null,
+	Descripcion varchar(150) null,
+	CostoEstimado int not null check(CostoEstimado >= 0),
+	FechaInicio date not null,
+	FechaFin date null,
+	Estado bit not null default(1)	
+)
+GO
+
+CREATE TABLE Modulos (
+	ID int not null primary key identity(1,1),
+	Nombre varchar(30) not null,
+	Descripcion varchar(100) null,
+	IDProyecto varchar(5) not null foreign key references Proyectos(ID),
+	TiempoEstimado int not null check(TiempoEstimado>0),
+	CostoEstimado int not null check(CostoEstimado>0),
+	FechaInicio date null,
+	FechaEstimadaFin date null,
+	FechaFin date null
+)
+GO
+
+CREATE TABLE Paises(
+	ID tinyint not null primary key identity (1,1),
+	Nombre varchar(20)
+)
+GO
+
+CREATE TABLE Ciudades(
+	ID int not null primary key identity(1,1),
+	IdPais tinyint foreign key references Paises(ID),
+	Nombre varchar(30)
+)
+GO
+
+CREATE TABLE Colaboradores(
+	ID int not null primary key identity(1,1),
+	Apellido varchar(40) not null,
+	Nombre varchar(40) not null, 
+	EMail varchar(60) null,
+	Celular varchar(15) null,
+	FechaNacimiento date not null,
+	Tipo char(1) not null check(Tipo = 'I' OR Tipo = 'E'),
+	Domicilio varchar(50) null,
+	IDCiudad int null foreign key references Ciudades(ID)
+)
+GO
+
+ALTER TABLE Modulos ADD CONSTRAINT CHK_FechaEstimadaFin CHECK(FechaEstimadaFin >= FechaInicio)
+ALTER TABLE Modulos ADD CONSTRAINT CHK_FechaFin CHECK(FechaFin >= FechaInicio)
+GO
+
+ALTER TABLE Clientes ADD IDCiudad INT NULL foreign key references Ciudades(ID)
+GO
+
+ALTER TABLE Colaboradores ADD CONSTRAINT CHK_MailOCelular CHECK (EMail is not null or Celular is not null)
+GO
+
+CREATE TABLE TiposTarea(
+	ID int not null primary key identity(1,1),
+	Nombre varchar(50) not null,
+	PrecioHoraBase money null
+)
+GO
+
+CREATE TABLE Tareas(
+	ID int not null Primary key identity(1,1),
+	IdModulo int not null foreign key references Modulos(ID),
+	IdTipo int not null foreign key references TiposTarea(ID),
+	FechaInicio datetime null,
+	FechaFin datetime null,
+	Estado bit not null default(1)
+)
+GO
+
+ALTER TABLE Tareas ADD CONSTRAINT CHK_FechaFinTareas CHECK(FechaFin>=FechaInicio)
+GO
+
+CREATE TABLE Colaboraciones(
+	ID bigint not null primary key identity(1,1),
+	IdTarea int not null foreign key references Tareas(ID),
+	IdColaborador int not null foreign key references Colaboradores(ID),
+	PrecioHora smallmoney not null check(PrecioHora > 0),
+	Tiempo smallint null check(Tiempo > 0),
+	Estado bit not null default(1)
+)
